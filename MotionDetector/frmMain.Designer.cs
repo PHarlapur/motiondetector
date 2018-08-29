57c294fba9fa5d53e3ad6ddb19d4f91acd22d181
Prabhas HARLAPUR <pharlapur@markem-imaje.com>
2018-08-29 14:59:23 +0530

    Added motion detector file.

---
+++ MotionDetector/frmMain.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Detector
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDetect = new System.Windows.Forms.Button();
+            this.lblMotionTimeValue = new System.Windows.Forms.Label();
+            this.lblMotionTime = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnDetect
+            // 
+            this.btnDetect.Location = new System.Drawing.Point(12, 31);
+            this.btnDetect.Name = "btnDetect";
+            this.btnDetect.Size = new System.Drawing.Size(139, 23);
+            this.btnDetect.TabIndex = 0;
+            this.btnDetect.Text = "Start Motion Detection";
+            this.btnDetect.UseVisualStyleBackColor = true;
+            this.btnDetect.Click += new System.EventHandler(this.btnDetect_Click);
+            // 
+            // lblMotionTimeValue
+            // 
+            this.lblMotionTimeValue.AutoSize = true;
+            this.lblMotionTimeValue.Location = new System.Drawing.Point(181, 67);
+            this.lblMotionTimeValue.Name = "lblMotionTimeValue";
+            this.lblMotionTimeValue.Size = new System.Drawing.Size(0, 13);
+            this.lblMotionTimeValue.TabIndex = 1;
+            // 
+            // lblMotionTime
+            // 
+            this.lblMotionTime.AutoSize = true;
+            this.lblMotionTime.Location = new System.Drawing.Point(15, 67);
+            this.lblMotionTime.Name = "lblMotionTime";
+            this.lblMotionTime.Size = new System.Drawing.Size(112, 13);
+            this.lblMotionTime.TabIndex = 2;
+            this.lblMotionTime.Text = "Last Motion Detected:";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(175, 31);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(140, 23);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "Close Motion Detection";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 104);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.lblMotionTime);
+            this.Controls.Add(this.lblMotionTimeValue);
+            this.Controls.Add(this.btnDetect);
+            this.Name = "frmMain";
+            this.Text = "Motion Detector";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDetect;
+        private System.Windows.Forms.Label lblMotionTimeValue;
+        private System.Windows.Forms.Label lblMotionTime;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ MotionDetector/frmMain.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge.Vision.Motion;
+using System.IO;
+using System.Runtime.InteropServices;
+using AForge.Controls;
+
+namespace Detector
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDetect_Click(object sender, EventArgs e)
+        {
+            this.ValidateDection();
+            this.btnDetect.Enabled = false;
+        }
+
+
+
+        private static MotionDetector _motionDetector;
+        private static float _motionAlarmLevel = 0.01f;
+        private static bool _hasMotion;
+        private static int _volgnr;
+        private static string _path;
+        VideoSourcePlayer videoSourcePlayer;
+        private delegate void UpdateStatusDelegate();
+
+        public void ValidateDection()
+        {
+            _path = Path.GetTempPath();
+
+            Console.WriteLine("Motion Detector");
+
+            Console.WriteLine("Detects motion in the integrated laptop webcam");
+
+            Console.WriteLine("Threshold level: " + _motionAlarmLevel);
+
+            _motionDetector = new MotionDetector(new TwoFramesDifferenceDetector(), new MotionAreaHighlighting());
+
+            if (new FilterInfoCollection(FilterCategory.VideoInputDevice).Count > 0)
+            {
+
+
+                var videoDevice = new FilterInfoCollection(FilterCategory.VideoInputDevice)[0];
+                var videoCaptureDevice = new VideoCaptureDevice(videoDevice.MonikerString);
+                videoSourcePlayer = new VideoSourcePlayer();
+                videoSourcePlayer.NewFrame += VideoSourcePlayer_NewFrame;
+                videoSourcePlayer.VideoSource = new AsyncVideoSource(videoCaptureDevice);
+                videoSourcePlayer.Start();
+            }
+        }
+
+        private  void VideoSourcePlayer_NewFrame(object sender, ref System.Drawing.Bitmap image)
+        {
+            var motionLevel = _motionDetector.ProcessFrame(image);
+
+            if (motionLevel > _motionAlarmLevel)
+            {
+                if (_hasMotion) return;
+
+                this.Invoke(new UpdateStatusDelegate(this.UpdateLabel));
+
+
+
+                _volgnr++;
+                _hasMotion = true;
+
+                PowerHelper.ForceSystemAwake();
+                PowerHelper.ResetSystemDefault();
+            }
+            else
+            {
+                if (_hasMotion)
+                {
+                    Console.WriteLine(DateTime.Now + ": Motion stopped. Motion level: " + motionLevel);
+                }
+                _hasMotion = false;
+            }
+        }
+
+        public void UpdateLabel()
+        {
+           
+            this.lblMotionTimeValue.Text = DateTime.Now.ToString("HH:mm");
+
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            videoSourcePlayer.Stop();
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            videoSourcePlayer.Stop();
+            this.btnDetect.Enabled = true;
+        }
+    }
+
+    public class PowerHelper
+    {
+        public static void ForceSystemAwake()
+        {
+            NativeMethods.SetThreadExecutionState(NativeMethods.EXECUTION_STATE.ES_CONTINUOUS |
+                                                  NativeMethods.EXECUTION_STATE.ES_DISPLAY_REQUIRED |
+                                                  NativeMethods.EXECUTION_STATE.ES_SYSTEM_REQUIRED |
+                                                  NativeMethods.EXECUTION_STATE.ES_AWAYMODE_REQUIRED);
+        }
+
+        public static void ResetSystemDefault()
+        {
+            NativeMethods.SetThreadExecutionState(NativeMethods.EXECUTION_STATE.ES_CONTINUOUS);
+        }
+    }
+
+    internal static class NativeMethods
+    {
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
+
+        [FlagsAttribute]
+        public enum EXECUTION_STATE : uint
+        {
+            ES_AWAYMODE_REQUIRED = 0x00000040,
+            ES_CONTINUOUS = 0x80000000,
+            ES_DISPLAY_REQUIRED = 0x00000002,
+            ES_SYSTEM_REQUIRED = 0x00000001
+
+            // Legacy flag, should not be used.
+            // ES_USER_PRESENT = 0x00000004
+        }
+    }
+
+}
